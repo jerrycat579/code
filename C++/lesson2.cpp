3e1e8d7e550fd24cab01af43dcc599d007b56534
xiheeeeee <1239214893@qq.com>
2025-03-18 18:06:53 +0800

    c语言进制输出

--- C++/lesson2.cpp
+++ C++/lesson2.cpp
@@ -5,23 +5,6 @@ int ans[maxn];
 
 int main()
  {  
-  int T, n;  
-  memset(ans, 0, sizeof(ans));//将最大容量为manx的数组a全部填入0
-  for(int m = 1; m < maxn; m++) 
-  {  
-   int x = m, y = m;  
-   while(x > 0) 
-   { 
-    y += x % 10; 
-    x /= 10; 
-   }  
-   if(ans[y] == 0 || m < ans[y]) ans[y] = m;
-  }  
-  scanf("%d", &T);  
-  while(T--)
-   {  
-    scanf("%d", &n);   
-    printf("%d\n", ans[n]); 
-   } 
+  printf("%d\n%o\n%x\n",15,15,15);
   return 0;  
  }
